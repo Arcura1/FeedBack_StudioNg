26cd687e3a81941aee3e5e3da027f67090660204
Red <95549175+TheRedSS@users.noreply.github.com>
2024-12-07 20:12:01 +0300

    berke

--- src/app/feedback/component/Profile/profle.component.ts
+++ src/app/feedback/component/Profile/profle.component.ts
@@ -1,9 +1,20 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector:"profile",
   templateUrl:"profle.component.html"
 })
-export class ProfileComponent{
+export class ProfileComponent implements OnInit {
+  user: any = {}; // Kullanıcı bilgilerini saklamak için değişken
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // `mainPage` bileşeninden oturumdaki kullanıcı bilgilerini al
+    this.user = JSON.parse(sessionStorage.getItem('user') || '{}');
+
+    console.log('Kullanıcı ID:', this.user.id);
+    console.log('Kullanıcı Adı:', this.user.firstName);
+    console.log('Kullanıcı Rolü:', this.user.role);
+  }
 }

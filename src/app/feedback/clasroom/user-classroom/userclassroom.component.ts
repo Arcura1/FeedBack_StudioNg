e1331cb97095f2a1f76822b82d569f2ac3ea107e
Oray isik <87896994+Arcura1@users.noreply.github.com>
2025-05-17 15:13:51 +0300

    update

--- src/app/feedback/clasroom/user-classroom/userclassroom.component.ts
+++ src/app/feedback/clasroom/user-classroom/userclassroom.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ClassroomUser } from "./queryModel/ClassroomUser";
 import { ClassroomUserService } from "./service/service";
 import { ClassroomService } from "../../component/Teacher/service/classroom.service";
-import { debounceTime, switchMap } from "rxjs";
+import {debounceTime, Subject, switchMap} from "rxjs";
 import { FormControl } from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-classroom-user',
@@ -15,10 +16,51 @@ export class UserclassroomComponent implements OnInit {
   classrooms: { id: number; name: string }[] = [];
   searchControl = new FormControl('');
 
-  constructor(
+
+  searchText: string = '';
+  filteredUsers: any[] = [];
+  selectedUserId: number | null = null;
+
+  private searchSubject = new Subject<string>();
+
+
+  constructor(private http: HttpClient,
     private classroomService: ClassroomService,
     private classroomUserService: ClassroomUserService
-  ) {}
+  ) {
+
+    this.searchSubject.pipe(debounceTime(300)).subscribe(searchText => {
+      console.log(searchText)
+      this.searchUsers(searchText);
+    });
+  }
+
+  onSearchChange() {
+    this.searchSubject.next(this.searchText);
+  }
+
+  searchUsers(query: string) {
+    const body = {
+      firstName: query,
+    };
+
+    console.log(body)
+    console.log(query)
+
+    this.http.post<any[]>('http://localhost:8080/api/users/search', body).subscribe(users => {
+      console.log(users)
+      this.filteredUsers = users;
+    });
+  }
+
+  onUserSelect(user: any) {
+    this.selectedUserId = user.id;
+    this.filteredUsers = [];
+    this.formData.userId=user.id;
+
+    this.searchText = `${user.firstName} ${user.lastName}`;
+    console.log('Seçilen Kullanıcı ID:', this.selectedUserId);
+  }
 
   ngOnInit() {
     this.loadUsers();
--- src/app/feedback/feedback.module.ts
+++ src/app/feedback/feedback.module.ts
@@ -10,7 +10,7 @@ import { StudentComponent } from './component/student/student.component';
 import {PdfEditComponent} from "./component/pdfEditPage/pdfEdit.component";
 import {TeacherComponent} from "./component/Teacher/teacher.component";
 import {PopupComponent} from "./component/popup/popup.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {PopupStudentComponent} from "./component/student/popupstudent/popupstudent.component";
 import {OrganizationComponent} from "./organization/organization.component";
 import {ClassroomComponent} from "./clasroom/classroom.component";
@@ -26,6 +26,10 @@ import {RolePageComponent} from "./userManagment/rolePage/rolePage.component";
 import {AuthorityRoleComponent} from "./userManagment/authorityRole/authorityRole.component";
 import {AuthorityPageComponent} from "./userManagment/authorityPage/authorityPage.component";
 import {UserPageComponent} from "./userManagment/userPage/userPage.component";
+import {UserclassroomComponent} from "./clasroom/user-classroom/userclassroom.component";
+import {CustomPageComponent} from "./component/custom/customPage.component";
+import {QrcodeComponent} from "./component/qrcode/qrcode.component";
+import {QRCodeModule} from "angularx-qrcode";
 
 
 
@@ -62,7 +66,7 @@ import {UserPageComponent} from "./userManagment/userPage/userPage.component";
     CustomPageComponent,
     HomeworkComponent,
     GuestComponent,
-    QrcodeComponent // ✅ buraya eklendi
+    QrcodeComponent, // ✅ buraya eklendi
     UserclassroomComponent,
     GuestComponent
   ],
@@ -77,6 +81,8 @@ import {UserPageComponent} from "./userManagment/userPage/userPage.component";
     CommonModule,
     FeedbackRoutingModule,
     FormsModule,
+    QRCodeModule,
+    ReactiveFormsModule,
 
 
   ]
